1d6a206641dcd9d097bf9ee2cee3d2399027394f
darshanabk <darshanabk1112@gmail.com>
2025-02-26 02:20:31 +0530

    Updated run.py

---
+++ run.py
@@ -0,0 +1,8 @@
+# imports
+import Streamlit
+
+if __name__ == '__main__':
+    # Create an instance of the TrendsInDataJobs class
+    app = Streamlit
+    # Run the Streamlit application
+    app.main()
