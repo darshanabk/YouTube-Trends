d2ce7c8c888762d58e43fff9262762d7765c0402
Darshana Balakannan <74397181+darshanabk@users.noreply.github.com>
2025-04-29 21:15:34 +0530

    Updated refresh button

--- Streamlit/Streamlit.py
+++ Streamlit/Streamlit.py
@@ -820,6 +820,9 @@ def streamlitSideBar(file):
         tuple: Selected filter options including Continents, Countries, Category, Years, Channel Names, and Licensed Content.
     """
     st.sidebar.header("Filter")
+    if st.sidebar.button("🔁 Refresh"):
+        st.cache_data.clear()
+        st.experimental_rerun()
     file = file.sort_values(by = 'continent', ascending = True)
     continents = file['continent'].unique()
     continents= np.append(continents, 'All')
